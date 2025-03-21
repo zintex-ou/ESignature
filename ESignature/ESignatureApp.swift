2886b52c3f08575ed6aa59eae42a437a7b9f3287
solveit-arseniy-varitski <arseniy.varitski@solveit.dev>
2025-03-21 09:27:12 +0300

    Initial Commit

---
+++ ESignature/ESignatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESignatureApp.swift
+//  ESignature
+//
+//  Created by Arseniy on 21.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ESignatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
